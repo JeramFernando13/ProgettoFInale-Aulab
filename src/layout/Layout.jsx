fc6cda576a05a25e5dbaffce418ae1cb9b94076b
Jeram Fernando <jerry131104@gmail.com>
2025-04-14 15:47:09 +0200

    mobile

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -8,18 +8,21 @@ import { Toaster } from "react-hot-toast";
 export default function Layout() {
     return (
         <>
-            <Toaster position='top-center'reverseOrder={true} />
-            <div className = "flex min-h-full ">
-                <aside className="min-h-full ">
-                    <Sidebar />
-                </aside>
+            <div className="overflow-x-hidden max-w-screen">
 
-                <div className="flex-1 flex flex-col">
-                    <Header />
-                    <main className="flex-1 p-6">
-                        <Outlet />
-                    </main>
-                    <Footer />
+                <Toaster position='top-center'reverseOrder={true} />
+                <div className = "flex min-h-full ">
+                    <aside className="min-h-full ">
+                        <Sidebar />
+                    </aside>
+
+                    <div className="flex-1 flex flex-col">
+                        <Header />
+                        <main className="flex-1 p-6">
+                            <Outlet />
+                        </main>
+                        <Footer />
+                    </div>
                 </div>
             </div>
         </>
